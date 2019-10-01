c463ccc549a1430ce8b4079f56b91fbdd9e479a1
Joel Dennning <joeldennning@Joels-MacBook-Pro.local>
2019-10-01 08:33:12 -0600

    Self review

--- bin/packmap-cli.js
+++ bin/packmap-cli.js
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
-const program = require('commander')
-const version = require('../package.json').version
-const packmap = require('../lib/packmap.js')
+const program = require("commander");
+const version = require("../package.json").version;
+const packmap = require("../lib/packmap.js");
 
-program.version(version)
-program.option('-o, --outdir <outdir>', 'change the output directory', 'dist')
-program.option('p, --package <package>', 'path to package.json file')
+program.version(version);
+program.option("-o, --outdir <outdir>", "change the output directory", "dist");
+program.option("p, --package <package>", "path to package.json file");
 
-program.parse(process.argv)
+program.parse(process.argv);
 
-const initialTime = new Date().getTime()
+const initialTime = new Date().getTime();
 packmap(program)
-.then(() => {
-  const totalTime = new Date().getTime() - initialTime
-  console.log(`packmap finished in ${totalTime}ms. Check the ${program.outdir} directory.`)
-})
-.catch(err => {
-  console.error(err)
-  process.exit(1)
-})
+  .then(() => {
+    const totalTime = new Date().getTime() - initialTime;
+    console.log(
+      `packmap finished in ${totalTime}ms. Check the ${program.outdir} directory.`
+    );
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
